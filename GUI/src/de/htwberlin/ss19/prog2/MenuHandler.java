7b6af5280641b048b76775000c80706b3a06f28b
Mohammed AbuJarour <mohammed.abujarour@htw-berlin.de>
2019-06-17 01:59:25 +0200

    Add fileChooser

--- GUI/src/de/htwberlin/ss19/prog2/MenuHandler.java
+++ GUI/src/de/htwberlin/ss19/prog2/MenuHandler.java
@@ -3,14 +3,26 @@ package de.htwberlin.ss19.prog2;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JFileChooser;
+import javax.swing.JMenuItem;
+
 public class MenuHandler implements ActionListener {
 
-	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+
 		Object source = e.getSource();
-		
+
 		System.out.println(source);
+		JMenuItem menuItem = (JMenuItem) source;
+		if (menuItem.getName().contains("Open")) {
+			JFileChooser chooser = new JFileChooser();
+			int result = chooser.showOpenDialog(null);
+
+			if (result == JFileChooser.APPROVE_OPTION) {
+				System.out.println("Opening file: " + chooser.getSelectedFile().getPath());
+			}
+
+		}
 
 	}
 
