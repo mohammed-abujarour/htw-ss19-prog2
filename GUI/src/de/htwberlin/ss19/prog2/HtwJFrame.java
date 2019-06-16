c0ad4487cbd3ea09a9cc0b203917f5764e4621bf
Mohammed AbuJarour <mohammed.abujarour@htw-berlin.de>
2019-06-17 00:11:00 +0200

    Basic settings

--- GUI/src/de/htwberlin/ss19/prog2/HtwJFrame.java
+++ GUI/src/de/htwberlin/ss19/prog2/HtwJFrame.java
@@ -3,5 +3,13 @@ package de.htwberlin.ss19.prog2;
 import javax.swing.JFrame;
 
 public class HtwJFrame extends JFrame{
+	private String title = "JFrame Magic";
+	private int width = 600;
+	private int height = 400;
 
+	public HtwJFrame() {
+		setTitle(title);
+		setSize(width, height);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
