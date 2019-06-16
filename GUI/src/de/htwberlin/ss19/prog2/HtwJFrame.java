7f333ffdc01a166d41e835ea369a49216d9e0c60
Mohammed AbuJarour <mohammed.abujarour@htw-berlin.de>
2019-06-17 00:24:44 +0200

    Add Actionlistener

--- GUI/src/de/htwberlin/ss19/prog2/HtwJFrame.java
+++ GUI/src/de/htwberlin/ss19/prog2/HtwJFrame.java
@@ -1,11 +1,14 @@
 package de.htwberlin.ss19.prog2;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class HtwJFrame extends JFrame{
+public class HtwJFrame extends JFrame implements ActionListener{
 	private String title = "JFrame Magic";
 	private int width = 600;
 	private int height = 400;
@@ -22,6 +25,19 @@ public class HtwJFrame extends JFrame{
 	private JPanel init() {
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.YELLOW);
+		
+		JButton button = new JButton("Click me!");
+		button.addActionListener(this);
+		
+		panel.add(button);
+		
 		return panel;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		System.out.println(e.getSource());
+		
 	}
 }
