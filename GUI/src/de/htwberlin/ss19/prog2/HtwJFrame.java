f9c522d2016a1af6576e69b4cb688b0551703671
Mohammed AbuJarour <mohammed.abujarour@htw-berlin.de>
2019-06-17 00:17:00 +0200

    Add Panel

--- GUI/src/de/htwberlin/ss19/prog2/HtwJFrame.java
+++ GUI/src/de/htwberlin/ss19/prog2/HtwJFrame.java
@@ -1,6 +1,9 @@
 package de.htwberlin.ss19.prog2;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class HtwJFrame extends JFrame{
 	private String title = "JFrame Magic";
@@ -11,5 +14,14 @@ public class HtwJFrame extends JFrame{
 		setTitle(title);
 		setSize(width, height);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel htwPanel = init(); //preparePanel();
+		this.add(htwPanel);
+	}
+
+	private JPanel init() {
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.YELLOW);
+		return panel;
 	}
 }
