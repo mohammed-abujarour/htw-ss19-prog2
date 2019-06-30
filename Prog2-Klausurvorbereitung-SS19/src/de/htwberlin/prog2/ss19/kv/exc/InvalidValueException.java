f00cb1c8b1832c12ba44ef625085633c186181b5
Mohammed AbuJarour <mohammed.abujarour@htw-berlin.de>
2019-06-30 21:28:38 +0200

    Create Product class and  InvalidValueException

---
+++ Prog2-Klausurvorbereitung-SS19/src/de/htwberlin/prog2/ss19/kv/exc/InvalidValueException.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package de.htwberlin.prog2.ss19.kv.exc;
+
+/**
+ * @author Mohammed
+ *
+ */
+public class InvalidValueException extends Exception {
+
+	public InvalidValueException(String message) {
+		super(message);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5956824997570530891L;
+
+}
---
+++ Prog2-Klausurvorbereitung-SS19/src/de/htwberlin/prog2/ss19/kv/model/Product.java
@@ -0,0 +1,47 @@
+package de.htwberlin.prog2.ss19.kv.model;
+
+import de.htwberlin.prog2.ss19.kv.exc.InvalidValueException;
+
+public class Product {
+	
+	private String name;
+	private String category;
+	private double price;
+	
+	
+	public Product(String name, String category, double price) throws Exception{
+		super();
+		if(name.isEmpty())
+			throw new InvalidValueException("Name cannot be empty");
+		else
+			this.name = name;
+		
+		if(category.isEmpty())
+			throw new InvalidValueException("Category cannot be empty");
+		else
+			this.category = category;
+		if(price <= 0)
+			throw new InvalidValueException("Price must be greater than zero");
+		else
+			this.price = price;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public double getPrice() {
+		return price;
+	}
+
+	@Override
+	public String toString() {
+		return name + " (" + category + ") - " + price ;
+	}
+	
+	
+	
+
+}
