15e6af37a51ea92482313193384b9963b0add902
Mohammed AbuJarour <mohammed.abujarour@htw-berlin.de>
2019-06-30 21:48:54 +0200

    Add Starter class with Main method

---
+++ Prog2-Klausurvorbereitung-SS19/src/de/htwberlin/prog2/ss19/kv/Starter.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package de.htwberlin.prog2.ss19.kv;
+
+import de.htwberlin.prog2.ss19.kv.ui.ProductsFrame;
+
+/**
+ * @author Mohammed
+ *
+ */
+public class Starter {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+
+		ProductsFrame frame = new ProductsFrame();
+		frame.setVisible(true);
+
+	}
+
+}
